70cdc556f58ef3d2bb6b8e8d6ef28a901a36bb8b
Eirik00 <eirik.rene@gmail.com>
2022-01-12 18:19:22 +0100

    Homework

---
+++ Heimavinna/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Heimavinna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = 0;
+
+            List<string> problemsInput = new List<string>();
+            problemsInput = Console.ReadLine().Split(";").ToList();
+
+            for(int i = 0; i < problemsInput.Count(); i++)
+            {
+                try
+                {
+                    int.Parse(problemsInput[i]);
+                    numb += 1;
+                }
+                catch
+                {
+                    string[] split = problemsInput[i].Split("-");
+                    numb += 1+(int.Parse(split[1]) - int.Parse(split[0]));
+                }
+            }
+            Console.WriteLine(numb);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Kattis Problems <img align="right" src="https://open.kattis.com/images/kattis/judge.png?7f7dbf=">
 **This is just my repo for kattis problems that i attempt to complete, they are probably unefficient**
 
-* Current Score: 20.9
-* Rank:         30966
+* Current Score: 25.5
+* Rank:         26967
 
 ## Solved Problems
 * :white_check_mark: ACM(Air Conditioned Minions)
 * :white_check_mark: Aaah?
 * :white_check_mark: Data Planner
+* :white_check_mark: Homework(Heimavinna)
 * :white_check_mark: Jumbo Javelin
 * :white_check_mark: Odd Echo
 * :white_check_mark: Rating Problems
