6d1de5310bb94ceb547da387a5ffde69f6ffa01e
Eirik00 <eirik.rene@gmail.com>
2022-01-17 10:27:34 +0100

    yeah

---
+++ 0-1 Squences/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _0_1_Squences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string newString = "";
+            foreach (char h in input)
+                if(h.ToString() == "?")
+                    newString += h;
+            Console.WriteLine(1000000007 % Math.Pow(2,newString.Length));
+        }
+    }
+}
---
+++ N-sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace N_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeats = int.Parse(Console.ReadLine());
+            string[] integers = Console.ReadLine().Split(" ");
+            int sum = 0;
+            for(int i = 0; i < repeats; i++)
+            {
+                sum += int.Parse(integers[i]);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
